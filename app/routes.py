9dbc28bb9fa88991dd0c53cd74e99e73183aefe6
Andrew Wilks-Millard <a.wilksmillard2003@gmail.com>
2025-07-11 16:06:04 -0400

    Templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'hello world!'
+    user = {'username': 'Andrew'}
+    posts = [
+        {
+            'author': {'username': 'George'},
+            'body': 'Beautiful day in Grand Rapids!'
+        },
+        {
+            'author': {'username': 'Marshall'},
+            'body': 'The F1 movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
