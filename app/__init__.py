98911d3a65b136b9ff9177cea1554173fc7413f2
Andrew Wilks-Millard <a.wilksmillard2003@gmail.com>
2025-07-16 14:54:44 -0400

    I18n and L10n

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from config import Config
-from flask import Flask
+from flask import Flask, request
+from flask_babel import Babel, lazy_gettext as _l
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_migrate import Migrate
@@ -9,14 +10,20 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 import logging
 import os
 
+def get_locale():
+    # return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return 'es'
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 moment = Moment(app)
+babel = Babel(app, locale_selector=get_locale)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
--- app/email.py
+++ app/email.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from flask_babel import _
 from flask_mail import Message
 from threading import Thread
 from app import app, mail
@@ -15,7 +16,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_email('[Flask Mega] Reset Your Password',
+    send_email(_('[Flask Mega] Reset Your Password'),
                sender=app.config['ADMINS'][0],
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt',
